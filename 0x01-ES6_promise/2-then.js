411f1d7a6509e027affe9cfe5511306cab6c4e9e
Oluwatobilba <oluwateezzy03@gmail.com>
2023-07-25 16:53:52 +0100

    update

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then((data) => {
+      console.log("Got a response from the API");
+      return data;
+    })
+    .catch((error) => {
+      console.log("Got a response from the API");
+      return error;
+    });
+}
