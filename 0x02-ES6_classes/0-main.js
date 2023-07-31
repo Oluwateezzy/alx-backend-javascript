b752d8da10f8a591e55d39319bee2168bd8da5cb
Oluwatobilba <oluwateezzy03@gmail.com>
2023-07-31 17:40:10 +0100

    update

--- 0x02-ES6_classes/0-main.js
+++ 0x02-ES6_classes/0-main.js
@@ -1,4 +1,4 @@
 import HolbertonCourse from './2-hbtn_course';
 
-const course = new HolbertonCourse("PHP", 20, ["Lucie", "Guillaume"]);
+const course = new HolbertonCourse('PHP', 20, ['Lucie', 'Guillaume']);
 course.name = 10;
--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,13 +1,13 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== "string") {
-      throw new TypeError(`Name must be a string`);
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
-    if (typeof length !== "number") {
-      throw new TypeError(`Length must be a number`);
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
     }
-    if (typeof students !== "object") {
-      throw new TypeError(`Students must be a Object`);
+    if (typeof students !== 'object') {
+      throw new TypeError('Students must be a Object');
     }
 
     this._name = name;
@@ -16,18 +16,18 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
-    if (typeof newName === "string") {
+    if (typeof newName === 'string') {
       this._name = newName;
     } else {
-      throw new TypeError(`Name must be a string`);
+      throw new TypeError('Name must be a string');
     }
   }
 
   set length(newLength) {
-    if (typeof newLength === "number") {
+    if (typeof newLength === 'number') {
       this._length = newLength;
     } else {
-      throw new TypeError(`Length must be a number`);
+      throw new TypeError('Length must be a number');
     }
   }
 
@@ -35,7 +35,7 @@ export default class HolbertonCourse {
     if (Array.isArray(newStudents)) {
       this._students = newStudents;
     } else {
-      throw new TypeError(`Students must be a Object`);
+      throw new TypeError('Students must be a Object');
     }
   }
 
