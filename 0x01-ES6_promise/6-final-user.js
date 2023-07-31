6205a93a4295890a39158a69ef51d754ad4dbb22
Oluwatobilba <oluwateezzy03@gmail.com>
2023-07-31 12:26:16 +0100

    update

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,18 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    const result = [];
+    values.forEach((value) => {
+      if (value.status === 'fulfilled') {
+        result.push(value);
+      } else {
+        result.push({ status: value.status, value: `Error ${value.reason}` });
+      }
+    });
+  });
+}
