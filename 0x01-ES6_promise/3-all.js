623de0c4bbf01706e22071ecb70ff4ab333b655e
Oluwatobilba <oluwateezzy03@gmail.com>
2023-07-31 11:39:30 +0100

    update

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,14 @@
-import { createUser, uploadPhoto } from "./utils";
+import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([createUser(), uploadPhoto()]).then(
     (result) => {
       console.log(
-        `${result[1].body} ${result[0].firstName} ${result[0].lastName}`
+        `${result[1].body} ${result[0].firstName} ${result[0].lastName}`,
       );
     },
     () => {
-      console.log("Signup system offline");
-    }
+      console.log('Signup system offline');
+    },
   );
 }
