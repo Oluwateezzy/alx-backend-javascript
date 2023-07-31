c7d77ed08957bc0a38d446a8b984307ef6a017e7
Oluwatobilba <oluwateezzy03@gmail.com>
2023-07-31 11:30:58 +0100

    update

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([createUser(), uploadPhoto()]).then((result) => {
+  Promise.all([createUser(), uploadPhoto()]).then((result) => {
     console.log(
       `${result[1].body} ${result[0].firstName} ${result[0].lastName}`,
     );
