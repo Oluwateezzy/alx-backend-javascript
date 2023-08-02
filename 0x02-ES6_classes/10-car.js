9fb0fbc0834a00e6865e12aa877ceb9c138a59d3
Oluwatobilba <oluwateezzy03@gmail.com>
2023-08-02 14:39:30 +0100

    update

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,39 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  set brand(brand) {
+    this._brand = brand;
+  }
+
+  set motor(motor) {
+    this._motor = motor;
+  }
+
+  set color(color) {
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    return new this.constructor[Symbol.species]();
+  }
+}
