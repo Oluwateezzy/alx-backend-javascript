ecece703da94241c0521d02fd5e25d8029d197bf
Oluwatobilba <oluwateezzy03@gmail.com>
2023-08-02 13:33:04 +0100

    update

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,21 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    if (this.constructor !== Building) {
-      if (this.evacuationWarningMessage !== 'function') {
-        throw new Error(
-          'Class extending Building must override evacuationWarningMessage',
-        );
-      }
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage === 'undefined'
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
     }
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Abstract method must be overridden in the subclass.');
   }
 }
