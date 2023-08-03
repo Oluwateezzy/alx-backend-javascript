5da222b36b259ba5d3d5265965fef789336be9bc
Oluwatobilba <oluwateezzy03@gmail.com>
2023-08-03 11:41:25 +0100

    update

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,3 +1,9 @@
 export default function groceriesList() {
-  return new Map([])
+  return new Map([
+    ['apple', 3],
+    ['banana', 5],
+    ['orange', 2],
+    ['avocado', 1],
+    ['pear', 4],
+  ]);
 }
