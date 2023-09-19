9747a085e2cc8371d3266719e82ff9e2e66a0505
Oluwatobilba <oluwateezzy03@gmail.com>
2023-09-19 16:33:17 +0100

    update

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!\n')
+})
+
+const PORT = 1245
+app.listen(PORT)
+
+module.exports = app
