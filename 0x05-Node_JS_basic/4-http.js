3861fd6d9ab081babbd7c80fd024d39dd194684c
Oluwatobilba <oluwateezzy03@gmail.com>
2023-09-19 15:18:09 +0100

    update

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,10 @@
 const http = require('http');
 // const app = hhtp()
 const app = http.createServer((req, res) => {
-    res.statusCode = 200
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!\n');
 });
 
 app.listen(1245, '127.0.0.1');
-module.exports = app
+module.exports = app;
