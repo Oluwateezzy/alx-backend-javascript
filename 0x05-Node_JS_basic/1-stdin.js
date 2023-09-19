9b912d844e9c733f95be54dae97c8c4b6ba67628
Oluwatobilba <oluwateezzy03@gmail.com>
2023-09-19 13:52:38 +0100

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
+process.stdin.on('data', (chunk) => {
+  const name = chunk.toString().trim();
   if (name) {
-    process.stdout.write(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name}\n`);
   }
 });
 
