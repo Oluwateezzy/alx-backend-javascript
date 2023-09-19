30d573bcacbc797ec9013c8acc86baae42bc631d
Oluwatobilba <oluwateezzy03@gmail.com>
2023-09-19 22:19:34 +0100

    update

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -5,7 +5,7 @@ class StudentsController {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
     response.write('This is the list of our students\n');
-    readDatabase('./database').then((data) => {
+    readDatabase('./database.csv').then((data) => {
       response.write(`Number of students in CS: ${data.CS.length}. List: ${data.CS.join(', ')}\n`);
       response.write(`Number of students in SWE: ${data.SWE.length}. List: ${data.SWE.join(', ')}\n`);
       response.end();
@@ -16,18 +16,16 @@ class StudentsController {
   }
   static getAllStudentsByMajor(request, response) {
     response.statusCode = 200
-    const { major } = request.param
-    readDatabase('./database').then((data) => {
-        if (major === 'CS') {
-            response.write(`List: ${data.CS.join(', ')}\n`)
-            response.end()
-        } else if (major === 'SWE') {
-            response.write(`List: ${data.SWE.join(', ')}\n`)
-            response.end()
-        } else {
-            response.statusCode = 500
-            throw new Error("Major parameter must be CS or SWE")
-        }
+    const { major } = request.params
+    if (major !== 'CS' && major !== 'SWE') {
+            response.statusCode = 500;
+            response.write('Major parameter must be CS or SWE\n');
+            response.end();
+            return;
+    }
+    readDatabase('./database.csv').then((data) => {
+        response.write(`List: ${data[major].join(', ')}\n`);
+        response.end();
     }).catch(() => {
         response.statusCode = 500
         throw new Error("Cannot load the database")
---
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = require('./routes/index')
+const app = express()
+
+app.use('/', router)
+app.use('/students', router)
+app.use('/students/:major', router)
+
+app.listen(1245, () => console.log("Server is connected"))
+export default app;
